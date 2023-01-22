50b5c5cec3d781f471f17aae377b9440cbaeb856
Victor Chan <vctrchn@hotmail.ca>
2023-01-22 18:46:15 -0500

    finished contact page

--- src/contact.js
+++ src/contact.js
@@ -1,3 +1,25 @@
-const generateContactContent = () => {};
+const generateContactContent = () => {
+  const contactBtn = document.getElementById("contact");
+  contactBtn.classList.remove("inactive");
+  contactBtn.classList.add("active");
+
+  const contactContent = document.createElement("div");
+  contactContent.classList.add("contactContent");
+
+  const infoBox = document.createElement("div");
+  infoBox.classList.add("infoBox");
+  const infoTitle = document.createElement("h1");
+  infoTitle.textContent = "Shiro-chan";
+  const contactImg = new Image();
+  contactImg.src = "../src/assets/shiro.jpg";
+  contactImg.classList.add("contactImage");
+  const infoPhone = document.createElement("div");
+  infoPhone.textContent = "Phone: 111-111-1111 Email: shiro-chan@shiro.com";
+  infoBox.append(infoTitle, contactImg, infoPhone);
+
+  contactContent.appendChild(infoBox);
+
+  return contactContent;
+};
 
 export { generateContactContent };
